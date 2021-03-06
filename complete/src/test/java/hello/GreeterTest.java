86eceb2aa3d92f7925d04e46c45b91cc9bcca2a4
drvdevops2805 <75531566+drvdevops2805@users.noreply.github.com>
2021-03-06 17:27:53 +0530

    Update GreeterTest.java

--- complete/src/test/java/hello/GreeterTest.java
+++ complete/src/test/java/hello/GreeterTest.java
@@ -11,7 +11,7 @@ public class GreeterTest {
 
 	@Test
 	public void greeterSaysHello() {
-		assertThat(greeter.sayHello(), containsString("yello"));
+		assertThat(greeter.sayHello(), containsString("hello"));
 	}
 
 }
